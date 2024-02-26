db7beca7a5f4c0969b48a8f84f396a8006025fb2
Sabeer <94005086+SRC-Sabeer@users.noreply.github.com>
2024-02-26 18:20:40 +0500

    register

--- Main/Staionary Management system/Controllers/HomeController.cs
+++ Main/Staionary Management system/Controllers/HomeController.cs
@@ -1,19 +1,81 @@
-using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Identity;
 using Staionary_Management_system.Models;
-using System.Diagnostics;
+using Staionary_Management_system.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
 
 namespace Staionary_Management_system.Controllers
 {
-    public class HomeController : Controller
-    {
-        private readonly ILogger<HomeController> _logger;
+	public class HomeController : Controller
+	{
+		private readonly SignInManager<ApplicationUser> _signInManager;
+		private readonly UserManager<ApplicationUser> _userManager;
 
-        public HomeController(ILogger<HomeController> logger)
-        {
-            _logger = logger;
-        }
+		public HomeController(
+			UserManager<ApplicationUser> userManager,
+			SignInManager<ApplicationUser> signInManager)
+		{
+			_userManager = userManager;
+			_signInManager = signInManager;
+		}
 
-        public IActionResult Index()
+		public IActionResult Login_Register()
+		{
+			return View();
+		}
+
+		[HttpPost]
+		public async Task<IActionResult> Login(LoginViewModel model)
+		{
+			if (ModelState.IsValid)
+			{
+				var result = await _signInManager.PasswordSignInAsync(model.Email, model.Password, model.RememberMe, lockoutOnFailure: false);
+				if (result.Succeeded)
+				{
+					return RedirectToAction("Index", "Home");
+				}
+				else
+				{
+					ModelState.AddModelError(string.Empty, "Invalid login attempt.");
+					return View("Login_Register", model);
+				}
+			}
+			return View("Login_Register", model);
+		}
+
+		[HttpPost]
+		public async Task<IActionResult> Register(RegisterViewModel model)
+		{
+			if (ModelState.IsValid)
+			{
+				var user = new ApplicationUser
+				{
+					FirstName = model.FirstName,
+					LastName = model.LastName,
+					UserName = model.Email,
+					Email = model.Email,
+					DateCreated = DateTime.Now
+				};
+
+				var result = await _userManager.CreateAsync(user, model.Password);
+				if (result.Succeeded)
+				{
+					await _signInManager.SignInAsync(user, isPersistent: false);
+					return RedirectToAction("Index", "Home");
+				}
+				foreach (var error in result.Errors)
+				{
+					ModelState.AddModelError("", error.Description);
+				}
+			}
+			return View("Login_Register", model);
+		}
+	}
+
+
+
+
+public IActionResult Index()
         {
             return View();
         }
@@ -73,11 +135,7 @@ namespace Staionary_Management_system.Controllers
             return View();
         }
 
-        public IActionResult Login_Register()
-        {
-            return View();
-        }
-
+     
         public IActionResult LookBook()
         {
             return View();
@@ -141,4 +199,3 @@ namespace Staionary_Management_system.Controllers
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
     }
-}
